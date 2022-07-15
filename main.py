46782282d89e619e061cbbbdd01c9d08a4844d30
Md Zunayedul Islam <znyd28@gmail.com>
2022-07-15 21:51:46 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from pytube import YouTube
+
+url = input('URL: ')
+yt = YouTube(url)
+
+print("Title:", yt.title)
+print("YouTuber:", yt.author)
+print("Published:", yt.publish_date)
+
+command = input("Audio only? [y/n]: ")
+
+if command == 'y':
+    audio = yt.streams.filter(only_audio=True).first()
+    file = audio.download("/Users/zunayed/Desktop/Projects/Python-Projects/YouTube-Downloader/Downloaded Audios/")
+    base, ext = os.path.splitext(file)
+    new_file = base + '.mp3'
+    os.rename(file, new_file)
+    print("Done")
+    
+else:
+    res = input("Resolution? [best/1080/720/480/360]: ")
+    if res == "best":
+            video = yt.streams.get_highest_resolution()
+            video.download("/Users/zunayed/Desktop/Projects/Python-Projects/YouTube-Downloader/Downloaded Videos/")
+    video = yt.streams.get_by_resolution(res + 'p')
+    video.download("/Users/zunayed/Desktop/Projects/Python-Projects/YouTube-Downloader/Downloaded Videos/")
+    print("Done")
+
